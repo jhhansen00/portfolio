024cef04c35055a0d1670b7ad2fa4799f8faf73c
unknown <jhhansen00@gmail.com>
2022-03-17 16:26:51 -0500

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/about/about";
 import Intro from "./components/intro/Intro";
+import Projects from "./components/projects/projects";
 
 function App() {
   return (
     <div>
       <Intro />
       <About />
+      <Projects />
     </div>
   );
 }
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,7 +3,18 @@ import "./projects.css"
 function Projects () {
     return (
         <div className="p">
-
+            <div className="p-left-bg"></div>
+            <div className="p-center-bg">
+                <div className="p-title-wrap">
+                    <div className="p-title">
+                        Projects
+                        <hr className="p-title-line"/>
+                    </div>
+                </div>
+            </div>
+            <div className="p-right-bg"></div>
         </div>
     )
 }
+
+export default Projects;
