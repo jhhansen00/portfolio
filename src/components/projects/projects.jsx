adc9f5067be1ee86107bea5db08d9c7385b6db9f
unknown <jhhansen00@gmail.com>
2022-03-18 01:11:21 -0500

    third component

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,16 +3,14 @@ import "./projects.css"
 function Projects () {
     return (
         <div className="p">
-            <div className="p-left-bg"></div>
-            <div className="p-center-bg">
-                <div className="p-title-wrap">
-                    <div className="p-title">
-                        Projects
-                        <hr className="p-title-line"/>
-                    </div>
-                </div>
+            <div className="p-left">
+                <div className="p-left-bg"></div>
+                <div className="p-left-bg2"></div>
+                <div className="p-left-bg3"></div>
             </div>
-            <div className="p-right-bg"></div>
+            <div className="p-right">
+                <div className="p-right-bg"></div>
+            </div>
         </div>
     )
 }
