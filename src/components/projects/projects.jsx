a13ecde8cbe58c9a7756160a41ca66067170a639
unknown <jhhansen00@gmail.com>
2022-03-21 20:01:04 -0500

    Git Employed

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,7 @@
 import "./projects.css"
 import connect4 from "../../img/connect 4.png"
 import twitter from "../../img/Twitter.png"
+import git from "../../img/GitEmployed.png"
 
 function Projects () {
     return (
@@ -11,7 +12,6 @@ function Projects () {
                 <div className="p-left-bg3"></div>
                 <div className="p-left-wrapper">
                     <h1 className="p-title">Projects:</h1>
-                    <br />
                     <div className="c4">
                         <h1 className="ptitles">Connect 4</h1>
                         <div>HTML, CSS, Javascript, Node.js</div>
@@ -23,13 +23,18 @@ function Projects () {
                         <div className="links"><a className="link" target="_blank" href="https://hardly-twitter.herokuapp.com/">Site</a> | <a className="link" target="_blank" href="https://github.com/jhhansen00/hardly-twitter">Github</a></div>
 
                     </div>
+                    <div className="ge">
+                        <h1 className="ptitles">Git Employed</h1>
+                        <div>Python, Django, PostgreSql, HTML, CSS</div>
+                        <div className="links"><a target="_blank" href="https://gitemployed.herokuapp.com/" className="link">Site</a> | <a href="https://github.com/Nexusmane/GitEmployed" className="link">Github</a></div>
+                    </div>
                 </div>
             </div>
             <div className="p-right">
                 <div className="p-right-bg"></div>
                 <a target="_blank" href="https://jhhansen00.github.io/connect_4/"><img className="c4img" src={connect4} alt="" /></a>
-                <a href="https://hardly-twitter.herokuapp.com/" target="_blank"><img className="htimg" src={twitter} alt="" /></a>
-
+                <a target="_blank" href="https://hardly-twitter.herokuapp.com/"><img className="htimg" src={twitter} alt="" /></a>
+                <a target="_blank" href="https://gitemployed.herokuapp.com/"><img className="geimg" src={git} alt="" /></a>
             </div>
         </div>
     )
